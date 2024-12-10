d89e88d92d15cf9392d4e450ef24cdc68460ce6c
Robbin de Groot <robbindegroot@gmail.com>
2024-12-10 07:30:05 +0100

    Added disconnect checking and handling for admin connections

--- message-queue/server/src/connection_manager.rs
+++ message-queue/server/src/connection_manager.rs
@@ -4,19 +4,23 @@ use backend::request::SetModeResponse;
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::mpsc::Sender;
 use std::sync::{Arc, Mutex};
-use std::{io, thread};
 use std::thread::JoinHandle;
+use std::{io, thread};
 
-pub struct ConnectionManager
-{
+pub struct ConnectionManager {
     listener: TcpListener,
     queue_manager: Arc<Mutex<QueueManager>>,
-    setup_connections: Mutex<Vec<(SocketAddr, Option<JoinHandle<(TcpStream, SetModeResponse)>>, Sender<()>)>>,
+    setup_connections: Mutex<
+        Vec<(
+            SocketAddr,
+            Option<JoinHandle<(TcpStream, SetModeResponse)>>,
+            Sender<()>,
+        )>,
+    >,
     admin_connections: Mutex<Vec<(SocketAddr, Option<JoinHandle<TcpStream>>, Sender<()>)>>,
 }
 
-impl ConnectionManager
-{
+impl ConnectionManager {
     pub fn new(listener: TcpListener, queue_manager: Arc<Mutex<QueueManager>>) -> Self {
         Self {
             listener,
@@ -32,8 +36,11 @@ impl ConnectionManager
                 Ok((stream, addr)) => {
                     println!("{addr}");
                     let (worker, interrupt) = SetupWorker::new(stream);
-                    let handle = thread::spawn(move || { worker.run() });
-                    self.setup_connections.lock().unwrap().push((addr, Some(handle), interrupt));
+                    let handle = thread::spawn(move || worker.run());
+                    self.setup_connections
+                        .lock()
+                        .unwrap()
+                        .push((addr, Some(handle), interrupt));
                     println!("connected");
                 }
                 Err(e) => {
@@ -51,28 +58,48 @@ impl ConnectionManager
             let handle = handle_opt.take().unwrap();
             if handle.is_finished() {
                 let (stream, termination) = handle.join().unwrap();
-                println!("{:?}", termination);
                 match termination {
                     SetModeResponse::Disconnect => println!("{} Disconnected", addr),
                     SetModeResponse::Sender(queue) => {
-                        self.queue_manager.lock().unwrap().connect_sender(&queue, stream)?;
+                        self.queue_manager
+                            .lock()
+                            .unwrap()
+                            .connect_sender(&queue, stream)?;
                     }
                     SetModeResponse::Receiver(queue) => {
-                        self.queue_manager.lock().unwrap().connect_receiver(&queue, stream);
+                        self.queue_manager
+                            .lock()
+                            .unwrap()
+                            .connect_receiver(&queue, stream);
                     }
                     SetModeResponse::Admin => {
-                        let (admin_worker, interrupt) = AdminWorker::new(self.queue_manager.clone(), stream);
-                        let admin_handle = thread::spawn(move || {
-                            admin_worker.run()
-                        });
-                        self.admin_connections.lock().unwrap().push((addr.clone(), Some(admin_handle), interrupt));
+                        let (admin_worker, interrupt) =
+                            AdminWorker::new(self.queue_manager.clone(), stream);
+                        let admin_handle = thread::spawn(move || admin_worker.run());
+                        self.admin_connections.lock().unwrap().push((
+                            addr.clone(),
+                            Some(admin_handle),
+                            interrupt,
+                        ));
                     }
                 }
             } else {
                 let _ = handle_opt.insert(handle);
             }
         }
-        self.setup_connections.lock().unwrap().retain(|(_, h, _)| h.is_some());
+        for (addr, handle_opt, _) in &mut self.admin_connections.lock().unwrap().iter_mut() {
+            let handle = handle_opt.take().unwrap();
+            if handle.is_finished() {
+                handle.join().unwrap();
+                println!("{} Disconnected", addr);
+            } else {
+                let _ = handle_opt.insert(handle);
+            }
+        }
+        self.setup_connections
+            .lock()
+            .unwrap()
+            .retain(|(_, h, _)| h.is_some());
         Ok(())
     }
 }
@@ -82,7 +109,7 @@ impl Drop for ConnectionManager {
         for (_, handle, interrupt) in &mut self.setup_connections.lock().unwrap().iter_mut() {
             // TODO handle errors
             interrupt.send(()).unwrap();
-            handle.take().unwrap().join().unwrap();  // Drop using "option dance"
+            handle.take().unwrap().join().unwrap(); // Drop using "option dance"
         }
     }
 }
