43c1fcd8eb2577d2eeab41f2ecf8371c1f14e011
Robbin de Groot <robbindegroot@gmail.com>
2024-12-12 17:23:33 +0100

    Refactored queue view by separating out table widget

--- message-queue/admin/src/main.rs
+++ message-queue/admin/src/main.rs
@@ -4,6 +4,7 @@ use crate::server_connector::ServerConnector;
 use backend::request::{CreateQueue, DeleteQueue, ListQueues};
 use iced::widget::{button, column, row, text, text_input, Column, Row};
 use iced::Alignment;
+use std::iter::zip;
 
 const QUEUE_VIEW_COLUMNS: [(&str, u16); 5] = [
     ("Queue", 200),
@@ -13,22 +14,6 @@ const QUEUE_VIEW_COLUMNS: [(&str, u16); 5] = [
     ("", 100),
 ];
 
-struct QueueView {
-    connector: ServerConnector,
-    queues: Vec<(String, usize, usize, usize)>,
-    new_queue_text: String,
-}
-
-impl Default for QueueView {
-    fn default() -> Self {
-        QueueView {
-            connector: ServerConnector::new(),
-            queues: Vec::default(),
-            new_queue_text: String::new(),
-        }
-    }
-}
-
 #[derive(Debug, Clone)]
 enum UIMessage {
     Refresh,
@@ -37,38 +22,87 @@ enum UIMessage {
     DeleteQueue(String),
 }
 
-impl QueueView {
-    pub fn view(&self) -> Column<UIMessage> {
-        let header: Row<UIMessage> = row(QUEUE_VIEW_COLUMNS.iter().map(|(name, width)| {
-            text!("{}", name)
-                .width(width.clone())
-                .align_x(Alignment::Center)
-                .into()
-        }));
+struct QueueTable {
+    names: [&'static str; 4],
+    widths: [u16; 4],
+    content: Vec<[String; 4]>,
+}
 
+impl QueueTable {
+    pub fn new(names: [&'static str; 4], widths: [u16; 4]) -> Self {
+        Self {
+            names,
+            widths,
+            content: vec![],
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.content.clear();
+    }
+
+    pub fn push(&mut self, row: (String, usize, usize, usize)) {
+        self.content.push([
+            row.0,
+            row.1.to_string(),
+            row.2.to_string(),
+            row.3.to_string(),
+        ]);
+        println!("content {:?}", self.content);
+    }
+
+    pub fn view(&self) -> Column<UIMessage> {
+        let header: Row<UIMessage> =
+            row(zip(self.names, self.widths.clone()).map(|(name, width)| {
+                text!("{}", name)
+                    .width(width)
+                    .align_x(Alignment::Center)
+                    .into()
+            }));
         let mut column: Column<UIMessage> = column![header];
-        println!("{:?}", self.queues);
-        if self.queues.len() <= 0 {
+        if self.content.len() <= 0 {
             column = column.push(
                 text("Nothing to see...")
                     .width(500)
                     .align_x(Alignment::Center),
             );
         } else {
-            for (queue, senders, receivers, messages) in &self.queues {
-                column = column.push(row![
-                    text(queue).width(QUEUE_VIEW_COLUMNS[0].1),
-                    text(senders).width(QUEUE_VIEW_COLUMNS[1].1),
-                    text(receivers).width(QUEUE_VIEW_COLUMNS[2].1),
-                    text(messages).width(QUEUE_VIEW_COLUMNS[3].1),
-                    button("Delete")
-                        .on_press(UIMessage::DeleteQueue(queue.clone())),
-                ]);
+            for row_content in &self.content {
+                let mut r: Row<UIMessage> =
+                    row(zip(self.widths, row_content).map(|(w, c)| text(c).width(w).into()));
+                r = r.push(
+                    button("Delete").on_press(UIMessage::DeleteQueue(row_content[0].clone())),
+                );
+                column = column.push(r);
             }
         }
+        column
+    }
+}
 
+struct QueueView {
+    connector: ServerConnector,
+    queue_table: QueueTable,
+    new_queue_text: String,
+}
+
+impl Default for QueueView {
+    fn default() -> Self {
+        QueueView {
+            connector: ServerConnector::new(),
+            queue_table: QueueTable::new(
+                ["Queue", "Senders", "Receivers", "Message"],
+                [300, 200, 200, 200],
+            ),
+            new_queue_text: String::new(),
+        }
+    }
+}
+
+impl QueueView {
+    pub fn view(&self) -> Column<UIMessage> {
         let mut cols = column![
-            column,
+            self.queue_table.view(),
             row![
                 text_input("new queue name", &self.new_queue_text)
                     .on_input(UIMessage::NewQueueName),
@@ -88,9 +122,9 @@ impl QueueView {
                 if let Ok(client) = self.connector.client() {
                     if let Ok(response) = client.transfer_admin_request(ListQueues {}) {
                         println!("ok {:?}", response);
-                        self.queues.clear();
+                        self.queue_table.clear();
                         for queue_data in response {
-                            self.queues.push(queue_data);
+                            self.queue_table.push(queue_data);
                         }
                     } else {
                         println!("err");
