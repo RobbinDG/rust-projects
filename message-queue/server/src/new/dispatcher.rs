120e27e0a5509b00431577b490a958e5e0d4c95b
Robbin de Groot <robbindegroot@gmail.com>
2025-01-21 14:23:25 +0100

    Added documentation to RequestDispatcher

--- message-queue/server/src/new/dispatcher.rs
+++ message-queue/server/src/new/dispatcher.rs
@@ -6,6 +6,7 @@ use backend::protocol::Request;
 use std::sync::{Arc, Mutex};
 use crate::new::request_handler::{CheckQueueHandler, CreateQueueHandler, DeleteQueueHandler, GetPropertiesHandler, Handler, ListQueuesHandler};
 
+/// A helper object to dispatch requests to a designated handler and encode their responses.
 pub struct RequestDispatcher {
     list_queues: ListQueuesHandler,
     check_queue: CheckQueueHandler,
@@ -15,6 +16,14 @@ pub struct RequestDispatcher {
 }
 
 impl RequestDispatcher {
+    /// Initialise a `RequestDispatcher`.
+    ///
+    /// # Arguments
+    ///
+    /// * `queue_store`: a shared reference to the queue store to modify by executing the
+    ///     requests that are dispatched using this dispatcher.
+    ///
+    /// returns: `RequestDispatcher`
     pub fn new(queue_store: Arc<Mutex<QueueStore>>) -> Self {
         Self {
             list_queues: ListQueuesHandler::new(queue_store.clone()),
@@ -25,6 +34,14 @@ impl RequestDispatcher {
         }
     }
 
+    /// Dispatch a supported request to the handler and return the encoded response (or error).
+    ///
+    /// # Arguments
+    ///
+    /// * `request`: a `SupportedRequest` that is to be dispatched.
+    ///
+    /// returns: `Result<Vec<u8, Global>, RequestError>` The byte-encoded result or a request
+    ///     error.
     pub async fn dispatch(&mut self, request: SupportedRequest) -> Result<Vec<u8>, RequestError> {
         match request {
             SupportedRequest::ListQueues(r) => handle_and_encode(r, &mut self.list_queues),
@@ -36,6 +53,15 @@ impl RequestDispatcher {
     }
 }
 
+/// A generic helper method to use a `Handler` instance to handle a `Request` and encode
+/// the response for a normalised output independent of generics.
+///
+/// # Arguments
+///
+/// * `request`: an implementor of `Request`; the request to handle.
+/// * `handler`: a corresponding `Handler` that can have `handle` called on `request`.
+///
+/// returns: `Result<Vec<u8, Global>, RequestError>` the encoded result or an error.
 fn handle_and_encode<R, H>(request: R, handler: &mut H) -> Result<Vec<u8>, RequestError>
 where
     R: Request,
