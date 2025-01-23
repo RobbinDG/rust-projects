6cf7ea3f6c163485a0e545188beba5b9a708dfe8
Robbin de Groot <robbindegroot@gmail.com>
2025-01-23 17:40:32 +0100

    Added TTL settings for message sending to UI

--- message-queue/admin/src/elements/inspect_view.rs
+++ message-queue/admin/src/elements/inspect_view.rs
@@ -1,15 +1,17 @@
 use crate::server_connector::ServerConnector;
+use crate::util::pretty_print_queue_dlx;
 use backend::protocol::message::{Message, TTL};
 use backend::protocol::queue_id::QueueId;
 use backend::protocol::request::{Publish, Receive};
 use backend::protocol::routing_key::{DLXPreference, RoutingKey};
 use backend::protocol::QueueProperties;
-use iced::widget::{button, column, horizontal_rule, row, text, text_input, vertical_rule};
+use iced::widget::{
+    button, checkbox, column, horizontal_rule, row, slider, text, text_input, vertical_rule,
+};
 use iced::{Alignment, Element, Length, Task};
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::sync::Mutex;
-use crate::util::pretty_print_queue_dlx;
 
 #[derive(Clone, Debug)]
 pub enum InspectViewMessage {
@@ -22,6 +24,8 @@ pub enum InspectViewMessage {
     ReceiveMessage,
     MessageReceived(String),
     NoMessageAvailable,
+    TTLValueChanged(u16),
+    TTLPermanentToggle(bool),
 }
 
 pub struct InspectView {
@@ -29,6 +33,8 @@ pub struct InspectView {
     message_body: String,
     received_message: String,
     connector: Arc<Mutex<ServerConnector>>,
+    ttl_value: u16,
+    ttl_permanent: bool,
 }
 
 impl InspectView {
@@ -38,6 +44,8 @@ impl InspectView {
             message_body: String::new(),
             received_message: String::new(),
             connector,
+            ttl_value: 50,
+            ttl_permanent: false,
         }
     }
 
@@ -66,26 +74,44 @@ impl InspectView {
                     row![
                         column![
                             text("Administration").align_x(Alignment::Center),
-                            text(format!("DLX: {}", pretty_print_queue_dlx(&properties.user.dlx))),
+                            text(format!(
+                                "DLX: {}",
+                                pretty_print_queue_dlx(&properties.user.dlx)
+                            )),
                             text(format!("Is DLX: {}", properties.user.is_dlx)),
-                            text(format!("Is System Managed: {}", properties.system.is_system)),
+                            text(format!(
+                                "Is System Managed: {}",
+                                properties.system.is_system
+                            )),
                             delete_btn,
                         ],
                         vertical_rule(1),
                         column![
                             text("Messaging").align_x(Alignment::Center),
+                            text_input("Message body", self.message_body.as_str())
+                                .on_input(InspectViewMessage::MessageBodyChanged),
                             row![
-                                text_input("Message body", self.message_body.as_str())
-                                    .on_input(InspectViewMessage::MessageBodyChanged),
+                                slider(
+                                    0..=300,
+                                    self.ttl_value,
+                                    InspectViewMessage::TTLValueChanged
+                                ),
+                                text(format!("{}s", self.ttl_value)),
+                                checkbox("Permanent", self.ttl_permanent)
+                                    .on_toggle(InspectViewMessage::TTLPermanentToggle),
                                 button("Send Message").on_press(InspectViewMessage::SendMessage)
-                            ].spacing(10),
+                            ]
+                            .spacing(10),
                             row![
                                 button("Receive Message")
                                     .on_press(InspectViewMessage::ReceiveMessage),
                                 text(self.received_message.as_str())
                             ],
-                        ].spacing(10),
-                    ].spacing(10).padding(10),
+                        ]
+                        .spacing(10),
+                    ]
+                    .spacing(10)
+                    .padding(10),
                 ]
                 .into();
                 element.map(Message::from)
@@ -107,6 +133,11 @@ impl InspectView {
                     let queue = queue.clone();
                     let connector = self.connector.clone();
                     let body = self.message_body.clone();
+                    let ttl = if self.ttl_permanent {
+                        TTL::Permanent
+                    } else {
+                        TTL::Duration(Duration::from_secs(self.ttl_value as u64))
+                    };
                     return Task::perform(
                         async move {
                             let mut binding = connector.lock().await;
@@ -121,7 +152,7 @@ impl InspectView {
                                             id: queue,
                                             dlx: DLXPreference::Default,
                                         },
-                                        ttl: TTL::Duration(Duration::from_secs(5)),
+                                        ttl,
                                     },
                                 })
                                 .await
@@ -165,6 +196,8 @@ impl InspectView {
             }
             InspectViewMessage::MessageReceived(m) => self.received_message = m,
             InspectViewMessage::NoMessageAvailable => self.received_message.clear(),
+            InspectViewMessage::TTLValueChanged(val) => self.ttl_value = val,
+            InspectViewMessage::TTLPermanentToggle(toggle) => self.ttl_permanent = toggle,
         }
         Task::none()
     }
