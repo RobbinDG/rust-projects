df5f6560292caa2fa5bfe43d7a7bcfc898a99f2a
Robbin de Groot <robbindegroot@gmail.com>
2025-05-09 20:27:23 +0200

    Now loading uploaded transactions into database

--- personal-finance-manager/migrations/20250509154457_transactions.sql
+++ personal-finance-manager/migrations/20250509154457_transactions.sql
@@ -1,4 +1,20 @@
 -- Add migration script here
 CREATE TABLE transactions (
-    test INTEGER
+    IBAN VARCHAR,
+    currency VARCHAR,
+    BIC VARCHAR,
+    MTCN INTEGER PRIMARY KEY,  -- Money Transfer Control Number
+    date DATE,
+    interest_date DATE,
+    value FLOAT,
+    balance_after FLOAT,
+    IBAN_other VARCHAR,
+    name_other VARCHAR,
+    BIC_other VARCHAR,
+    code VARCHAR,
+    reference VARCHAR,
+    description VARCHAR,
+    value_orig FLOAT,
+    currency_orig VARCHAR,
+    exchange_rate FLOAT
 )
--- personal-finance-manager/src/main.rs
+++ personal-finance-manager/src/main.rs
@@ -1,13 +1,13 @@
 #[macro_use]
 extern crate rocket;
 
-use std::io::Cursor;
 use rocket::fs::TempFile;
 use rocket::tokio::io::{AsyncBufReadExt, AsyncReadExt};
 use rocket::State;
 use sqlx::migrate::Migrator;
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
 use sqlx::{Pool, Sqlite};
+use std::io::Cursor;
 
 static MIGRATOR: Migrator = sqlx::migrate!(); // defaults to "./migrations"
 
@@ -17,7 +17,7 @@ fn hello(name: &str, age: u8) -> String {
 }
 
 #[post("/upload_transactions", format = "text/csv", data = "<file>")]
-async fn test(mut file: TempFile<'_>, /*, pool: &State<Pool<Sqlite>>*/) -> std::io::Result<String> {
+async fn test(mut file: TempFile<'_>, pool: &State<Pool<Sqlite>>) -> std::io::Result<String> {
     let mut contents = String::new();
     let mut open_file = match file.open().await {
         Err(e) => {
@@ -31,6 +31,53 @@ async fn test(mut file: TempFile<'_>, /*, pool: &State<Pool<Sqlite>>*/) -> std::
     }
 
     let mut rdr = csv::Reader::from_reader(Cursor::new(contents));
+
+    for row in rdr.records() {
+        match row {
+            Ok(row) => {
+                let r0 = row.get(0);
+                let r1 = row.get(1);
+                let r2 = row.get(2);
+                let r3 = row.get(3);
+                let r4 = row.get(4);
+                let r5 = row.get(5);
+                let r6 = row.get(6);
+                let r7 = row.get(7);
+                let r8 = row.get(8);
+                let r9 = row.get(9);
+                let r12 = row.get(12);
+                let r13 = row.get(13);
+                let r15 = row.get(15);
+                let r19 = row.get(19);
+                let r23 = row.get(23);
+                let r24 = row.get(24);
+                let r25 = row.get(25);
+                sqlx::query!(
+                    "INSERT OR IGNORE INTO transactions VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    r0,
+                    r1,
+                    r2,
+                    r3,
+                    r4,
+                    r5,
+                    r6,
+                    r7,
+                    r8,
+                    r9,
+                    r12,
+                    r13,
+                    r15,
+                    r19,
+                    r23,
+                    r24,
+                    r25,
+                ).execute(&**pool).await.unwrap();
+            }
+            Err(e) => {
+                return Ok(format!("CSV parse error: {}", e));
+            }
+        }
+    }
     Ok(if let Some(result) = rdr.records().next() {
         match result {
             Ok(record) => format!("First line: {:?}", record),
@@ -39,10 +86,6 @@ async fn test(mut file: TempFile<'_>, /*, pool: &State<Pool<Sqlite>>*/) -> std::
     } else {
         "Empty CSV file".to_string()
     })
-    // let mut buf = String::new();
-    // let x = file.open().await?.read_line(&mut buf).await?;
-    //
-    // Ok(buf)
 }
 
 #[launch]
