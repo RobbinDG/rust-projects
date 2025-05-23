dbb9409d3daee25f32134f0db83610e1da4b4c4c
Robbin de Groot <robbindegroot@gmail.com>
2025-05-23 08:45:22 +0200

    Added Dockerfile for API

--- personal-finance-manager/api/migrations/20250522140548_accounts.sql
+++ personal-finance-manager/api/migrations/20250522140548_accounts.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
 CREATE TABLE accounts (
-    iban VARCHAR PRIMARY KEY,
+    iban VARCHAR PRIMARY KEY NOT NULL,
     holder VARCHAR NOT NULL,
     name VARCHAR NOT NULL
 )
