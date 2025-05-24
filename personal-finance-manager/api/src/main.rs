c8ae85ea3f843c3421adb4cc744ab24104cd9fd6
Robbin de Groot <robbindegroot@gmail.com>
2025-05-24 10:33:51 +0200

    Added harder requirements on error handling in API

--- personal-finance-manager/api/src/main.rs
+++ personal-finance-manager/api/src/main.rs
@@ -1,3 +1,4 @@
+#![deny(clippy::unwrap_used, unused_must_use, let_underscore_drop)]
 #[macro_use]
 extern crate rocket;
 mod aggregates;
@@ -7,29 +8,26 @@ mod month_breakdown;
 mod transaction;
 mod accounts;
 
+use crate::accounts::get_accounts;
 use crate::aggregates::get_aggregates;
 use crate::categories::post_category;
 use crate::cors::CORS;
+use crate::month_breakdown::get_breakdown;
 use crate::transaction::{get_transactions, get_transactions_month, post_transactions, post_transactions_form};
 use rocket::serde::Serialize;
 use rocket::tokio::io::{AsyncBufReadExt, AsyncReadExt};
+use rocket::{Build, Rocket};
 use sqlx::migrate::Migrator;
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
-use std::env;
-use crate::accounts::get_accounts;
-use crate::month_breakdown::get_breakdown;
 
 static MIGRATOR: Migrator = sqlx::migrate!(); // defaults to "./migrations"
 
-#[launch]
-async fn rocket() -> _ {
-    dotenv::from_filename(".env").unwrap();
+async fn rocket_builder() -> Rocket<Build> {
+    dotenv::from_filename(".env").expect("Failed to load .env file");
 
     let options = SqliteConnectOptions::new()
-        .filename(dotenv::var("DATABASE_PATH").unwrap())
+        .filename(dotenv::var("DATABASE_PATH").expect("Couldn't load DATABASE_PATH"))
         .create_if_missing(true);
-
-    println!("{:?}", options);
 
     let pool = SqlitePoolOptions::new()
         .max_connections(5)
@@ -55,4 +53,9 @@ async fn rocket() -> _ {
             get_accounts,
         ],
     )
+}
+
+#[rocket::main]
+async fn main() {
+    rocket_builder().await.launch().await.expect("Failure to launch rocket");
 }
--- personal-finance-manager/api/src/transaction.rs
+++ personal-finance-manager/api/src/transaction.rs
@@ -1,10 +1,12 @@
 use chrono::NaiveDate;
+use csv::StringRecord;
 use rocket::form::Form;
 use rocket::fs::TempFile;
+use rocket::response::Responder;
 use rocket::serde::json::Json;
 use rocket::serde::Serialize;
 use rocket::tokio::io::AsyncReadExt;
-use rocket::State;
+use rocket::{Request, Response, State};
 use sqlx::{Pool, Sqlite, SqlitePool};
 use std::io::{Cursor, Error};
 
@@ -107,7 +109,7 @@ struct TransactionsUploadForm<'r> {
 pub async fn post_transactions_form(
     mut form: Form<TransactionsUploadForm<'_>>,
     pool: &State<Pool<Sqlite>>,
-) -> std::io::Result<String> {
+) -> Result<String, UploadError> {
     process_uploaded_tsv(&mut form.filename, pool).await
 }
 
@@ -115,88 +117,111 @@ pub async fn post_transactions_form(
 pub async fn post_transactions(
     mut file: TempFile<'_>,
     pool: &State<Pool<Sqlite>>,
-) -> std::io::Result<String> {
+) -> Result<String, UploadError> {
     process_uploaded_tsv(&mut file, pool).await
+}
+
+#[derive(Responder)]
+pub enum UploadError {
+    #[response(status = 500)]
+    FileOpenFailed(&'static str),
+    #[response(status = 500)]
+    FileReadFailed(&'static str),
+    #[response(status = 400)]
+    CSVParseFailed(&'static str),
 }
 
 async fn process_uploaded_tsv(
     file: &mut TempFile<'_>,
     pool: &State<Pool<Sqlite>>,
-) -> std::io::Result<String> {
+) -> Result<String, UploadError> {
     let mut contents = String::new();
     let mut open_file = match file.open().await {
-        Err(e) => {
-            return Ok(format!("Failed to open file: {}", e));
+        Err(_) => {
+            return Err(UploadError::FileOpenFailed("Failed to open file"));
         }
         Ok(f) => f,
     };
 
     if let Err(e) = open_file.read_to_string(&mut contents).await {
-        return Ok(format!("Failed to read file: {}", e));
+        return Err(UploadError::FileReadFailed("Failed to read file"));
     }
 
     let mut rdr = csv::Reader::from_reader(Cursor::new(contents));
 
+    let mut success_cnt = 0;
+    let mut failed_cnt = 0;
+
     for row in rdr.records() {
         match row {
             Ok(row) => {
-                let r0 = row.get(0);
-                let r1 = row.get(1);
-                let r2 = row.get(2);
-                let r3 = row.get(3);
-                let r4 = row.get(4);
-                let r5 = row.get(5);
-                let r6 = row
-                    .get(6)
-                    .and_then(|s| s.replace('.', "").replace(',', ".").parse::<f64>().ok());
-                let r7 = row.get(7).and_then(|s| {
-                    s.replace('.', "")
-                        .replace(',', ".")
-                        .replace('+', "")
-                        .parse::<f64>()
-                        .ok()
-                });
-                let r8 = row.get(8);
-                let r9 = row.get(9);
-                let r12 = row.get(12);
-                let r13 = row.get(13);
-                let r15 = row.get(15);
-                let r19 = row.get(19);
-                let r23 = row.get(23);
-                let r24 = row.get(24);
-                let r25 = row.get(25);
-                sqlx::query!(
-                    "INSERT OR IGNORE INTO transactions VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                    r0,
-                    r1,
-                    r2,
-                    r3,
-                    r4,
-                    r5,
-                    r6,
-                    r7,
-                    r8,
-                    r9,
-                    r12,
-                    r13,
-                    r15,
-                    r19,
-                    r23,
-                    r24,
-                    r25,
-                ).execute(&**pool).await.unwrap();
+                match insert_transaction(pool, row).await {
+                    Ok(_) => success_cnt += 1,
+                    Err(_) => failed_cnt += 1,
+                };
             }
             Err(e) => {
-                return Ok(format!("CSV parse error: {}", e));
+                return Err(UploadError::CSVParseFailed("Failed to parse CSV content"));
             }
         }
     }
-    Ok(if let Some(result) = rdr.records().next() {
-        match result {
-            Ok(record) => format!("First line: {:?}", record),
-            Err(e) => format!("CSV parse error: {}", e),
-        }
-    } else {
-        "Empty CSV file".to_string()
-    })
+
+    Ok(format!(
+        "Successfully inserted {}/{} (failed: {}) transactions",
+        success_cnt,
+        success_cnt + failed_cnt,
+        failed_cnt
+    ))
+}
+
+async fn insert_transaction(
+    pool: &State<Pool<Sqlite>>,
+    row: StringRecord,
+) -> Result<(), sqlx::Error> {
+    let r0 = row.get(0);
+    let r1 = row.get(1);
+    let r2 = row.get(2);
+    let r3 = row.get(3);
+    let r4 = row.get(4);
+    let r5 = row.get(5);
+    let r6 = row
+        .get(6)
+        .and_then(|s| s.replace('.', "").replace(',', ".").parse::<f64>().ok());
+    let r7 = row.get(7).and_then(|s| {
+        s.replace('.', "")
+            .replace(',', ".")
+            .replace('+', "")
+            .parse::<f64>()
+            .ok()
+    });
+    let r8 = row.get(8);
+    let r9 = row.get(9);
+    let r12 = row.get(12);
+    let r13 = row.get(13);
+    let r15 = row.get(15);
+    let r19 = row.get(19);
+    let r23 = row.get(23);
+    let r24 = row.get(24);
+    let r25 = row.get(25);
+    sqlx::query!(
+        "INSERT OR IGNORE INTO transactions VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        r0,
+        r1,
+        r2,
+        r3,
+        r4,
+        r5,
+        r6,
+        r7,
+        r8,
+        r9,
+        r12,
+        r13,
+        r15,
+        r19,
+        r23,
+        r24,
+        r25,
+    ).execute(&**pool).await?;
+    Ok(())
 }
